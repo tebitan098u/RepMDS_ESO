e1870710a53c44eaefcfd1ffe2cebf92b8a4a903
Esteban <esteban.soto@correounivalle.edu.co>
2023-10-02 15:34:29 -0500

    Se cambia logica

--- logica.js
+++ logica.js
@@ -1,5 +1,5 @@
 alert("Quiero visualizar esto en HTML");
 console.log("Quiero ver esto en la consola");
 alert("ejemplo");
-console.log("Estoy volviendo");
+console.log("Esto es un enunciado");
 console.log("Palabra")
