4a74e9d03539726df086b260ac11d3859fdca5e9
Esteban <esteban.soto@correounivalle.edu.co>
2023-10-02 15:53:01 -0500

    Se corrigio logica

--- logica.js
+++ logica.js
@@ -2,5 +2,6 @@ alert("Quiero visualizar esto en HTML");
 console.log("Quiero ver esto en la consola");
 alert("ejemplo");
 console.log("Esto es un enunciado");
-console.log("Palabra")
-console.log("Cambios en main")
+console.log("Palabra");
+console.log("Cambios en main");
+alert("Se va a crear un conflicto");
